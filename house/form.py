75a143376f8753870dec3f38a647930c9178cc62
tinhpham-nhaphangmy <tinhpham@nhaphangmy.com>
2020-04-24 17:21:53 +0700

    Rename variable

--- house/form.py
+++ house/form.py
@@ -1,16 +1,33 @@
 from django import forms
 
+
 class DemoForm(forms.Form):
-    sophong = forms.CharField(widget=forms.TextInput(attrs={'type':'number', 'name':'SoPhong', 'min':'1', 'max':'15', 'class':'span3 form-control', 'placeholder':'-----', 'required':''}))
-    sophongngu = forms.CharField(widget=forms.TextInput(attrs={'type':'number', 'name':'SoPhongNgu', 'min':'1', 'max':'15', 'class':'span3 form-control', 'placeholder':'-----', 'required':''}))
-    sophongtam = forms.CharField(widget=forms.TextInput(attrs={'type':'number','name':'SoPhongTam', 'min':'1', 'max':'10', 'class':'span3 form-control', 'placeholder':'-----', 'required':''}))
-    dientich = forms.CharField(widget=forms.TextInput(attrs={'type':'number','name':'DienTich', 'min':'1', 'max':'1000', 'step':'0.01', 'class':'span3 form-control', 'placeholder':'-----', 'required':''}))
-    namxaydung = forms.CharField(widget=forms.TextInput(attrs={'type':'number','name':'NamXayDung', 'min':'1800', 'max':'2019', 'class':'span3 form-control', 'placeholder':'-----', 'required':''}))
-    kc_trungtam = forms.CharField(widget=forms.TextInput(attrs={'type':'number', 'name':'KCTrungTam', 'min':'1', 'max':'', 'step':'0.01','class':'span3 form-control', 'placeholder':'-----', 'required':''}))
-    kc_sanbay = forms.CharField(widget=forms.TextInput(attrs={'type':'number', 'name':'KCSanBay', 'min':'1', 'max':'', 'step':'0.01','class':'span3 form-control', 'placeholder':'-----', 'required':''}))
-    kc_taudienngam = forms.CharField(widget=forms.TextInput(attrs={'type':'number', 'name':'KCTauDienNgam', 'min':'1', 'max':'','step':'0.01', 'class':'span3 form-control', 'placeholder':'-----', 'required':''}))
-    # art_expos = forms.CharField(widget=forms.TextInput(attrs={'type':'number', 'name':'Art_Expos', 'min':'1', 'max':'299', 'class':'span3 form-control', 'placeholder':'-----', 'required':''}))
-    
+    room_number_total = forms.CharField(widget=forms.TextInput(
+        attrs={'type': 'number', 'name': 'SoPhong', 'min': '1', 'max': '15', 'class': 'span3 form-control',
+               'placeholder': '-----', 'required': ''}))
+    bathroom_number = forms.CharField(widget=forms.TextInput(
+        attrs={'type': 'number', 'name': 'SoPhongNgu', 'min': '1', 'max': '15', 'class': 'span3 form-control',
+               'placeholder': '-----', 'required': ''}))
+    bedroom_number = forms.CharField(widget=forms.TextInput(
+        attrs={'type': 'number', 'name': 'SoPhongTam', 'min': '1', 'max': '10', 'class': 'span3 form-control',
+               'placeholder': '-----', 'required': ''}))
+    acreage = forms.CharField(widget=forms.TextInput(
+        attrs={'type': 'number', 'name': 'DienTich', 'min': '1', 'max': '1000', 'step': '0.01',
+               'class': 'span3 form-control', 'placeholder': '-----', 'required': ''}))
+    build_year = forms.CharField(widget=forms.TextInput(
+        attrs={'type': 'number', 'name': 'NamXayDung', 'min': '1800', 'max': '2019', 'class': 'span3 form-control',
+               'placeholder': '-----', 'required': ''}))
+    dist_to_center = forms.CharField(widget=forms.TextInput(
+        attrs={'type': 'number', 'name': 'KCTrungTam', 'min': '1', 'max': '', 'step': '0.01',
+               'class': 'span3 form-control', 'placeholder': '-----', 'required': ''}))
+    dist_to_airport = forms.CharField(widget=forms.TextInput(
+        attrs={'type': 'number', 'name': 'KCSanBay', 'min': '1', 'max': '', 'step': '0.01',
+               'class': 'span3 form-control', 'placeholder': '-----', 'required': ''}))
+    dist_to_station = forms.CharField(widget=forms.TextInput(
+        attrs={'type': 'number', 'name': 'KCTauDienNgam', 'min': '1', 'max': '', 'step': '0.01',
+               'class': 'span3 form-control', 'placeholder': '-----', 'required': ''}))
+
+
 class ContactForm(forms.Form):
     from_email = forms.EmailField(required=True)
     to_email = forms.EmailField(required=True)
--- house/linear_regression.py
+++ house/linear_regression.py
@@ -3,33 +3,35 @@ import numpy as np
 from sklearn import linear_model, metrics
 from sklearn.model_selection import train_test_split
 
+
 def getData():
     # Get home data from CSV file
     dataFile = pd.read_csv('house/data/mydata.csv')
     return dataFile
+
 
 def linearRegressionModel(X_train, Y_train, X_test, Y_test):
     linear = linear_model.LinearRegression()
     linear.fit(X_train, Y_train)
     score_trained = linear.score(X_test, Y_test)
     y_pre = linear.predict(X_test)
-    return linear.coef_,linear.intercept_,np.sqrt(metrics.mean_absolute_error(Y_test, y_pre)),score_trained
+    return linear.coef_, linear.intercept_, np.sqrt(metrics.mean_absolute_error(Y_test, y_pre)), score_trained
+
 
 def main():
-
     data = getData()
     if data is not None:
         # Selection few attributes
         attributes = list(
             [
-            'distance_to_citycenter',
-            'distance_to_airport',
-            'distance_to_station',
-            'year_built',
-            'num_room',
-            'num_bed',
-            'num_bath',
-            'living_area'
+                'distance_to_citycenter',
+                'distance_to_airport',
+                'distance_to_station',
+                'year_built',
+                'num_room',
+                'num_bed',
+                'num_bath',
+                'living_area'
             ]
         )
         # Vector attributes of house
@@ -38,7 +40,7 @@ def main():
         Y = data['askprice']
         # Split data to training test and testing test
         X_train, X_test, Y_train, Y_test = train_test_split(np.array(X), np.array(Y), test_size=0.2)
-        
-        a,b,c,d=linearRegressionModel(X_train, Y_train, X_test, Y_test)
-        
-        return a,b,c,d
+
+        a, b, c, d = linearRegressionModel(X_train, Y_train, X_test, Y_test)
+
+        return a, b, c, d
